150f63bdbd13804e53bbdff8196315a313867007
DMV921 <104264067+DMV921@users.noreply.github.com>
2023-03-18 18:03:08 +0300

    5 lesson
    5

--- src/lessons/lesson 5/profile/page-object/ProfileSettings.page.ts
+++ src/lessons/lesson 5/profile/page-object/ProfileSettings.page.ts
@@ -7,8 +7,7 @@ class ProfileSettingsPage {
     constructor(browser: WebdriverIO.Browser) {
         this.browser = browser
     }
-
-
+    //Name
     public async nameField(userName: string): Promise<void> {
         await this.getNameField().waitForDisplayed({
             timeoutMsg: 'Name field was not displayed',
@@ -25,8 +24,7 @@ class ProfileSettingsPage {
     public getUserNameText(): Promise<string> {
         return this.getNameField().getValue()
     }
-
-
+    //Bio
     public async bioField(userBio: string): Promise<void> {
         await this.getBioField().waitForDisplayed({
             timeoutMsg: 'Bio field was not displayed',
@@ -37,23 +35,96 @@ class ProfileSettingsPage {
     public getUserBioText(): Promise<string> {
         return this.getBioField().getValue()
     }
+    //Email
+    public async emailList(): Promise<void> {
+        await this.getProfileEmailList().waitForClickable({
+            timeoutMsg: 'Email list was not clickable',
+        })
+        await this.getProfileEmailList().selectByIndex(1)
+        await this.getUpdateProfileButton().click()
+    }
+    public getUserEmail(): Promise<string> {
+        return this.getProfileEmailList().getValue()
+    }
+    //Pronouns
+    public async pronounsList(): Promise<void> {
+        await this.getPronouns().waitForClickable({
+            timeoutMsg: 'Pronouns list was not clickable',
+        })
+        await this.getPronouns().selectByIndex(1)
+        await this.getUpdateProfileButton().click()
+    }
+    public getUserPronouns(): Promise<string> {
+        return this.getPronouns().getValue()
+    }
+    //Custom pronoun
+    public async customPronounsList(userCustomPronouns: string): Promise<void> {
+        await this.getPronouns().waitForClickable({
+            timeoutMsg: 'Pronouns list was not clickable',
+        })
+        await this.getPronouns().selectByIndex(4)
+        await this.getPronounsCustom().setValue(userCustomPronouns)
+        await this.getUpdateProfileButton().click()
+    }
+    public getCustomUserPronouns(): Promise<string> {
+        return this.getPronounsCustom().getValue()
+    }
+    //Picture
+    public async showHiddenFileInput(browser: WebdriverIO.Browser): Promise<void> {
+        await browser.execute(() => {
+            const htmlElement = document.querySelector('[type="file"]') as HTMLElement
+            htmlElement.style.cssText = 'display:block !important; opacity: 1; position: inherit;'
+        })
+    }
+    public async uploadFile(filePath: string): Promise<void> {
+        await this.getInputFile().waitForExist({
+            timeoutMsg: 'File input field was not exist',
+        })
+        await this.showHiddenFileInput(this.browser)
+        const file: string = await this.browser.uploadFile(filePath)
+        await this.getInputFile().setValue(file)
+        await this.getInputFileSetButton().waitForClickable({
+            timeoutMsg: 'Set new profile picture button was not clickable',
+        })
+        await this.getInputFileSetButton().click()
+        await this.getUpdateProfileButton().click()
+    }
+    public getNewAvatar(): Promise<string> {
+        return this.getAvatarsrc().getAttribute('src')
+    }
+    //Negative
+    //Name
+    public messegeLongName(): Promise<boolean> {
+        return this.getMessegeLongName().isDisplayed()
+    }
+   //Bio
+    public getBioLength(): Promise<string> {
+        return this.getBioField().getValue()        
+    }
+    //Picture
+    public async uploadBigSizeFile(filePath: string): Promise<void> {
+        await this.getInputFile().waitForExist({
+            timeoutMsg: 'File input field was not exist',
+        })
+        await this.showHiddenFileInput(this.browser)
+        const file: string = await this.browser.uploadFile(filePath)
+        await this.getInputFile().setValue(file)  
+        await this.getMessegeTooBig().waitForDisplayed({
+            timeoutMsg: 'The message "Please upload a picture smaller than 1 MB" was not displayed',
+        })
+    }
+    public messegeTooBig(): Promise<boolean> {
+        return this.getMessegeTooBig().isDisplayed()
+    }
 
 
+
+
+
+
+    //OpenProfilePage
     public async openProfilePage(): Promise<void> {
         await this.browser.url(this.url)
-    }
-
-    private getLoginField(): ChainablePromiseElement<WebdriverIO.Element> {
-        return this.browser.$('//*[@id="login_field"]')
-    }
-    private getPasswordField(): ChainablePromiseElement<WebdriverIO.Element> {
-        return this.browser.$('//*[@id="password"]')
-    }
-    private getLoginButton(): ChainablePromiseElement<WebdriverIO.Element> {
-        return this.browser.$('//*[@type="submit"]')
-    }
-    private getErrorMessege(): ChainablePromiseElement<WebdriverIO.Element> {
-        return this.browser.$('//*[@id="js-flash-container"]/div')
     }
 
     private getNameField(): ChainablePromiseElement<WebdriverIO.Element> {
@@ -64,13 +135,38 @@ class ProfileSettingsPage {
         return this.browser.$('//*[@id="user_profile_bio"]')
     }
 
+    private getProfileEmailList(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('//*[@id="user_profile_email"]')
+    }
+
+    private getPronouns(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('//*[@id="user_profile_pronouns_select"]')
+    }
+    private getPronounsCustom(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('//*[@id="user_profile_pronouns"]')
+    }
+    private getInputFile(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('[type="file"]')
+    }
+
+    private getInputFileSetButton(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('//*[@id="avatar-crop-form"]/div[2]/button/span')
+    }
+    private getAvatarsrc(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('//*[@id="settings-frame"]/div[2]/div[2]/dl/dd/div/details/summary/img')
+    }
+
+    private getMessegeLongName(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('//*[@id="js-flash-container"]/div/div/div')
+    }
+
+    private getMessegeTooBig(): ChainablePromiseElement<WebdriverIO.Element> {
+        return this.browser.$('//*[@id="settings-frame"]/div[2]/div[2]/dl/dd/form/file-attachment/div/div[2]')
+    }
+
     private getUpdateProfileButton(): ChainablePromiseElement<WebdriverIO.Element> {
         return this.browser.$('//*[@id="edit_user_104264067"]/div/p[2]/button')
     }
-
-
-
-
 
 }
 export {
--- src/lessons/lesson 5/profile/test/profile.test.ts
+++ src/lessons/lesson 5/profile/test/profile.test.ts
@@ -4,16 +4,22 @@ import { LoginPage } from '../page-object/Login.page'
 import { MainPage } from "../page-object/Main.page"
 import { ProfileSettingsPage } from "../page-object/ProfileSettings.page"
 
-
-const badEmail: string = 'fefefe'
-const badPassword: string = '1'
 const nameField: string = 'Дмитрий'
 const bioField: string = 'Информация обо мне.'
+const emailList: string = 'dmvltd1@gmail.com'
+const pronouns: string = 'they/them'
+const customPronouns: string = 'Custom'
+const srcWithoutAvatar: string = 'https://avatars.githubusercontent.com/u/104264067?s=400&v=4'
+const longNameField: string ='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis do'
+const longBioField: string ='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'
 
 describe('Profile settings test', () => {
     let loginPage: LoginPage
     let mainPage: MainPage
     let profileSettingsPage: ProfileSettingsPage
+    const filePath = 'src/files/placeimg_640_480_any.jpg'
+    const filePathBigSize = 'src/files/photo_visokogo_razresheniya.jpg'
+    
     before(async () => {
         loginPage = new LoginPage(browser)
         mainPage = new MainPage(browser)
@@ -26,54 +32,52 @@ describe('Profile settings test', () => {
         await profileSettingsPage.openProfilePage()
     })
 
-
     it('Name of user saved into name field', async () => {
         await profileSettingsPage.nameField(nameField)
         //await profileSettingsPage.clicknameField()
         expect(await profileSettingsPage.getUserNameText()).toEqual(nameField)
     })
 
-
     it('Biography of user saved into Bio field', async () => {
         await profileSettingsPage.bioField(bioField)
         expect(await profileSettingsPage.getUserBioText()).toEqual(bioField)
     })
 
-
-
-
-
-    /*
-
-    it('User will login using login', async () => {
-        await loginPage.login(LOGIN, PASSWORD)
-        await mainPage.openUserMenu()
-        expect(await mainPage.getUserLoginText()).toEqual(LOGIN)
+    it('Email of user saved into Public email list', async () => {
+        await profileSettingsPage.emailList()
+        expect(await profileSettingsPage.getUserEmail()).toEqual(emailList)
     })
 
-    it('User will login using email', async () => {
-        await loginPage.login(EMAIL, PASSWORD)
-        await mainPage.openUserMenu()
-        expect(await mainPage.getUserLoginText()).toEqual(LOGIN)
+    it('Pronoun of user saved into Pronouns list', async () => {
+        await profileSettingsPage.pronounsList()
+        expect(await profileSettingsPage.getUserPronouns()).toEqual(pronouns)
     })
 
-    it('Error message should be displayed by login with wrong email', async () => {
-        await loginPage.login(badEmail, PASSWORD)
-        await mainPage.openUserMenu()
-        expect(await mainPage.getUserLoginText()).toEqual(LOGIN)
+    it('Pronoun of user saved into custom field', async () => {
+        await profileSettingsPage.customPronounsList(customPronouns)
+        expect(await profileSettingsPage.getCustomUserPronouns()).toEqual(customPronouns)
     })
 
-    it('Error message should be displayed by login with wrong password', async () => {
-        await loginPage.login(LOGIN, badPassword)
+    it('Photo should be uploaded in profile', async () => {
+        await profileSettingsPage.uploadFile(filePath)
+        expect(await profileSettingsPage.getNewAvatar()).not.toEqual(srcWithoutAvatar)
+    })
+    //NEGATIVE
+    it('A message about the long name will appear', async () => {
+        await profileSettingsPage.nameField(longNameField)
+    
+        expect(await profileSettingsPage.messegeLongName()).toEqual(true)
+        expect(await profileSettingsPage.getUserNameText()).toEqual(nameField)
+    })
 
-        await browser.$('//*[@id="js-flash-container"]/div').isDisplayed()
-        expect(await browser.$('//*[@id="js-flash-container"]/div').isDisplayed()).toEqual(true)
+    it('Biography field limited to 160 characters', async () => {
+        await profileSettingsPage.bioField(longBioField)
+        expect(await profileSettingsPage.getBioLength()).toHaveLength(160)
+    })
 
+    it.only('The message "Please upload a picture smaller than 1 MB" should appear.', async () => {
+        await profileSettingsPage.uploadBigSizeFile(filePathBigSize)
+        expect(await profileSettingsPage.messegeTooBig()).toEqual(true)
+    })
 
-       })
-*/ 
-  //  afterEach(async () => {
- //       await browser.reloadSession()
-
-  //  })
 })
