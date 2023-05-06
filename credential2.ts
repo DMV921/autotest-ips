15f4a2b26ed42ad1f53303d0ff86b686c1beda7a
Дмитрий <dmvltd1@gmail.com>
2023-05-06 21:22:49 +0300

    Delete credential2.ts

--- credential2.ts
+++
@@ -1,5 +0,0 @@
-export const LOGIN = 'dDAdada111'
-export const EMAIL = 'ds8054296@gmail.com'
-export const PASSWORD = 'Gameplay92121'
-export const PERSONAL_TOKEN = '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'
-export const REPO ='Test'
