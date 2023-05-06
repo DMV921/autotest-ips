6830bd16a3b72193683d08a9106e007dde7e1dc2
Дмитрий <dmvltd1@gmail.com>
2023-05-06 21:22:57 +0300

    Delete credential3.ts

--- credential3.ts
+++
@@ -1,4 +0,0 @@
-export const LOGIN = 'dadadada1111'
-export const EMAIL = 'aqorden@gmail.com'
-export const PASSWORD = 'Gameplay92121'
-export const PERSONAL_TOKEN = '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'
